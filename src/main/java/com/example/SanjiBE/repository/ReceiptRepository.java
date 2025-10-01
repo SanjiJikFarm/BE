7765d421747a44ad61d701f683ab7308fc4e3547
Jiin <chloe02.han@gmail.com>
2025-10-02 00:23:08 +0900

    feat: Get receipt/receiptList implemented

--- src/main/java/com/example/SanjiBE/repository/ReceiptRepository.java
+++ src/main/java/com/example/SanjiBE/repository/ReceiptRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
     boolean existsByUserAndShopAndReceiptDateAndTotalPrice(User user, Shop shop, LocalDate receiptDate, int totalPrice);
+
+    List<Receipt> findByUser(User user);
 }
 
