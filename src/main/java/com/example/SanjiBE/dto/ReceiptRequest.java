28fa653370ae2fce5419137c84688f5d8240a55e
Jiin <chloe02.han@gmail.com>
2025-10-09 04:04:20 +0900

    feat: receipt api 수정

--- src/main/java/com/example/SanjiBE/dto/ReceiptRequest.java
+++ src/main/java/com/example/SanjiBE/dto/ReceiptRequest.java
@@ -1,19 +1,18 @@
 package com.example.SanjiBE.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.List;
 
 @Data
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReceiptRequest {
     private String storeName;
     private String date;
     private String totalAmount;
+
+    @JsonProperty("items")
     private List<ReceiptItemDto> itemList;
-
-    public ReceiptItemDto[] getItems() {
-        return itemList.toArray(new ReceiptItemDto[0]);
-    }
 }
-
