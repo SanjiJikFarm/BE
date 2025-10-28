996f2f8c256f77472713b08bb7be99d5fb976386
Jiin <chloe02.han@gmail.com>
2025-10-29 01:26:10 +0900

    fix: ai 기능 수정

--- src/main/java/com/example/SanjiBE/repository/LocalOriginRepository.java
+++ src/main/java/com/example/SanjiBE/repository/LocalOriginRepository.java
@@ -2,8 +2,11 @@ package com.example.SanjiBE.repository;
 
 import com.example.SanjiBE.entity.LocalOrigin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface LocalOriginRepository extends JpaRepository<LocalOrigin, Long> {
-    Optional<LocalOrigin> findFirstByItemNameContaining(String itemName);
+    Optional<LocalOrigin> findByItemNameContaining(String itemName);
 }
